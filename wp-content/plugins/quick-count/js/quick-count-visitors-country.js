02b0e413c6a65e6fb389d7f4ddee4aa8f1e1ed33
daibien <vovuongdaibien@gmail.com>
2015-12-25 16:10:20 +0700

    2512151610

--- wp-content/plugins/quick-count/js/quick-count-visitors-country.js
+++
@@ -1,1 +0,0 @@
-var quick_count=jQuery.extend(quick_count||{},{visitors_country_map:function(){var a=jQuery("div.quick-count-visitors-map");quick_count.map(a,quick_count.cdata,60)}},quick_count_visitors_country||{});jQuery(document).ready(function(){1==quick_count.qfc&&quick_count.visitors_country_map()});
