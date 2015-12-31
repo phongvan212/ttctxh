9f1a54c633d625711366bcf0be86301b389e653a
phongvan212 <phongvan212@gmail.com>
2015-12-31 15:46:29 +0700

    3112151546

--- wp-config.php
+++ wp-config.php
@@ -21,7 +21,7 @@ define( 'WP_CACHE', true );
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'WPCACHEHOME', 'C:\xampp\htdocs\ttctxh\wp-content\plugins\wp-super-cache/' ); //Added by WP-Cache Manager
+//define( 'WPCACHEHOME', 'C:\xampp\htdocs\ttctxh\wp-content\plugins\wp-super-cache/' ); //Added by WP-Cache Manager
 define('DB_NAME', 'dbttctxh');
 
 /** MySQL database username */
